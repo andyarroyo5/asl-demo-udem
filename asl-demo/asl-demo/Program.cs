66b85f6bf1f99fab63c2f8a618cf3dfb542e6a23
Andrea Arroyo <soarroyo@microsoft.com>
2020-03-10 00:47:08 -0600

    1. Create Console App Project

---
+++ asl-demo/asl-demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace asl_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
